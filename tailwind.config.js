f3ae41671db3622e6b21b7077561d0ae1bd84890
hanouka4 <hanouka4@gmail.com>
2026-05-16 11:43:41 +0300

    revert: return theme to previously working state to debug 404

--- tailwind.config.js
+++ tailwind.config.js
@@ -143,5 +143,6 @@ module.exports = {
     plugins: [
       require('@salla.sa/twilight-tailwind-theme'),
       require('@tailwindcss/forms'),
+      require('@tailwindcss/line-clamp'),
     ],
 }
